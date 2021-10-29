e2c03b3b807b4f38d4afaa9ac10d4dad69892e02
Nullius <junusov.eldiyar@gmail.com>
2021-10-29 15:28:21 +0600

    retur

---
+++ ProblemSet02/ArtCanvasNode_22_06/ArtCanvasNode_22_06.java
@@ -0,0 +1,22 @@
+package ProblemSet02.ArtCanvasNode_22_06;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ArtCanvasNode_22_06 extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("ArtCanvasNode_22_06.fxml"));
+        primaryStage.setTitle("Canvas");
+        primaryStage.setScene(new Scene(root, 500, 505));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
